9661b055fa68deb399fe306749201a846a333985
sandersoncorrea <correasanderson@gmail.com>
2019-09-05 16:10:57 -0300

    dados

--- reactjs/src/services/api.js
+++ reactjs/src/services/api.js
@@ -8,33 +8,33 @@ export function loadLists() {
           id: 1,
           content: "Estudar módulo 01 de NodeJS",
           labels: ["#7159c1"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars1.githubusercontent.com/u/1024025?s=400&v=4"
         },
         {
           id: 2,
           content:
             "Criar vídeo para o Youtube ensinando a recriar a interface do Pipefy",
           labels: ["#7159c1"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars1.githubusercontent.com/u/1024025?s=400&v=4"
         },
         {
           id: 3,
           content: "Estudar módulo 03 de React Native",
           labels: ["#7159c1"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars1.githubusercontent.com/u/83798?s=200&v=4"
         },
         {
           id: 4,
           content:
             'Gravar Aula "NextJS: Utilizando server-side rendering com ReactJS"',
           labels: ["#54e1f7"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars3.githubusercontent.com/u/12513172?s=460&v=4"
         },
         {
           id: 5,
           content: "Gravar testes e deploy ReactJS",
           labels: ["#54e1f7"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars3.githubusercontent.com/u/12513172?s=460&v=4"
         }
       ]
     },
@@ -46,7 +46,7 @@ export function loadLists() {
           id: 6,
           content: "Recriando clone do Pipefy",
           labels: [],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars3.githubusercontent.com/u/12513172?s=460&v=4"
         }
       ]
     },
@@ -58,18 +58,19 @@ export function loadLists() {
           id: 7,
           content: "Gravar sobre Geolocalização e mapas com React Native",
           labels: ["#7159c1"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars3.githubusercontent.com/u/12513172?s=460&v=4"
         },
         {
           id: 8,
           content: "Gravar testes e deploy ReactJS",
           labels: ["#54e1f7"],
-          user: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/profile.png"
+          user: "https://avatars3.githubusercontent.com/u/12513172?s=460&v=4"
         },
         {
           id: 9,
           content: "Ajustes na biblioteca unform",
-          labels: []
+          labels: [],
+          user: "https://avatars1.githubusercontent.com/u/83798?s=200&v=4"
         }
       ]
     },
@@ -81,18 +82,21 @@ export function loadLists() {
         {
           id: 10,
           content: "Gravar aula sobre deploy e CI com React Native",
-          labels: []
+          labels: [],
+          user: "https://avatars1.githubusercontent.com/u/83798?s=200&v=4"
         },
         {
           id: 12,
           content: "Gravar testes e deploy ReactJS",
-          labels: ["#54e1f7"]
+          labels: ["#54e1f7"],
+          user: "https://avatars3.githubusercontent.com/u/12513172?s=460&v=4"
         },
         {
           id: 13,
           content:
             'Gravar Aula "Internacionalização de aplicações Node.js, ReactJS e React Native"',
-          labels: ["#7159c1"]
+          labels: ["#7159c1"],
+          user: "https://avatars1.githubusercontent.com/u/83798?s=200&v=4"
         }
       ]
     }
