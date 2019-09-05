4dbe21ddc9c7b9fd6a9fffa1fd316b19c3b20e8c
sandersoncorrea <correasanderson@gmail.com>
2019-09-05 16:04:32 -0300

    Inicial

---
+++ reactjs/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { DndProvider } from "react-dnd";
+import Html5Backend from "react-dnd-html5-backend";
+
+import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <DndProvider backend={Html5Backend}>
+      <Header />
+      <Board />
+
+      <GlobalStyle />
+    </DndProvider>
+  );
+}
+
+export default App;
---
+++ reactjs/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function header() {
+  return (
+    <Container>
+      <h1>Pipefy Sand</h1>
+    </Container>
+  );
+}
